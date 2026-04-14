9d670367cd05756634f2858cdf42b899ba0f6736
1073500 <1073500@hr.nl>
2026-04-14 17:04:13 +0200

    id

--- agent.js
+++ agent.js
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import {AzureChatOpenAI} from "@langchain/openai"
 import {createAgent} from "langchain";
 import {getWeather, readDate} from "./tools.js"
@@ -13,22 +14,22 @@ const agent = createAgent({
 });
 
 //AGENT (aanroepen)
-async function callAgent(prompt) {
+export async function callAgent(prompt) {
     try {
         const result = await agent.invoke({
-            messages: [{role: "user", content: prompt}],
-        });
+            messages: [{role: "user", content: prompt}],},
+            { configurable: { thread_id: "1" } }
+        );
 
         //Last Item
         const finalMessage = result.messages.at(-1);
         console.log(finalMessage.content);
-        //return result;
+        return finalMessage.content;
     } catch (error) {
         console.error("Azure OpenAI error:", error);
         return "Sorry, the assistant is currently unavailable.";
     }
+
 }
 
 callAgent("What's the weather like in Amsterdam? And can you roll a 6-sided dice for me? And also, if you land on a 3 then tell me what date is it today.")
-
-
